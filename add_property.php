246343759b332c8bff5442bc8f9aaaf63644b7c9
hassanyf <mrxortax@gmail.com>
2016-05-28 14:30:52 +0500

    Added propert_add page

---
+++ add_property.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+		<!-- <link rel="stylesheet" type="text/css" href="signup.css"> -->
+<title>
+		Add Property
+	</title>
+</head>
+<body>
+	<form action="admin.php" method ="post" enctype="multipart/form-data">
+	<br><br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Model Name" name = "model" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Company Name" name = "company" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Dealer Name" name = "dealer" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Purchase Price" name = "price" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Category" name = "category" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Engine Capacity (CC)" name = "engine" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Seating Capacity (CC)" name = "seats" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Mileage" name = "mileage" value = ""></center>
+	<br><br>
+	<center><input type="text" class="css-input" placeholder="Enter Car Condition" name = "condition" value = ""></center>
+	<br><br>
+	<center><center><input type="file" name="myimage" id="myimage" class = "myButton"></center></center>
+	<br><br>
+	<h8>
+	<input type="submit" class = "myButton">
+	</h8>
+	</form>
+</body>
+</html>
