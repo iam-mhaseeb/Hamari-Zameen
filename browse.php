9cafd5202c07279e441ee0f0e2d56ffaf981a05b
hassanyf <mrxortax@gmail.com>
2016-05-29 10:32:15 +0500

    Added browse feature

--- browse.php
+++ browse.php
@@ -5,80 +5,76 @@ if (!isset($_SESSION['user']))
 		header("location: login.php");
 	}
 ?>
-<!DOCTYPE HTML>
-<!--
-	Linear by TEMPLATED
-    templated.co @templatedco
-    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-<html>
-	<head>
-	<style>
-	.cartable {
-		width:100%;
-		height:100%;
-		border:2px groove #C0C0C0;
-		border-collapse:collapse;
-		padding:5px;
-	}
-	.cartable th {
-		border:2px groove #C0C0C0;
-		padding:10px;
-		background:#F4FDF8;
-	}
-	.cartable td {
-		border:2px groove #C0C0C0;
-		padding:15px;
-		background:#F4FDF8;
-		font-size:15px;
-	}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Hamari Zameen | Project</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
 
-	</style>
-		<title>AutoHire</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,700,500,900' rel='stylesheet' type='text/css'>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-panels.min.js"></script>
-		<script src="js/init.js"></script>
-		<link rel="stylesheet" href="css/skel-noscript.css" />
-		<link rel="stylesheet" href="css/style.css" />
-		<link rel="stylesheet" href="css/style-desktop.css" />
-	</head>
-	<body>
+<body data-spy="scroll" data-target="#navbar" data-offset="0">
+    <header id="header" role="banner">
+        <div class="container">
+            <div id="navbar" class="navbar navbar-default">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"></a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#main-slider"><i class="icon-home"></i></a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#portfolio">Browse</a></li>
+                        <li><a href="admin.php">Admin</a></li>
+                        <li><a href="#about-us">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header><!--/#header-->
 
-	<!-- Header -->
-		<div id="header">
-			<div id="nav-wrapper"> 
-				<!-- Nav -->
-				<nav id="nav">
-					<ul>
-						<li class="active"><a href="home.php">Home</a></li>
-						<li><a href="car.php">Our Cars</a></li>
-						<li><a href="rent.php">Rent Cars</a></li>
-						<li><a href="taxi.php">Taxi Service</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href = "logout.php"><?php echo $_SESSION['user']. " : " ?>Logout</a></li>
-					</ul>
-				</nav>
-			</div>
-			<div class="container"> 
-				
-				<!-- Logo -->
-				<div id="logo">
-					<h1><a href="#">AutoHire</a></h1>
-				</div>
-			</div>
-		</div>
-	<!-- Header --> 
+    <section id="main-slider" class="carousel">
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="container">
+                    <div class="carousel-content">
+                        <h1>Free Onepage Theme</h1>
+                        <p class="lead">Xeon is the best free onepage responsive theme available arround the globe<br>Download it right now for free</p>
+                    </div>
+                </div>
+            </div><!--/.item-->
+            <div class="item">
+                <div class="container">
+                    <div class="carousel-content">
+                        <h1>ShapeBootstrap.net</h1>
+                        <p class="lead">Download free but 100% premium quaility twitter Bootstrap based WordPress and HTML themes <br>from shapebootstrap.net</p>
+                    </div>
+                </div>
+            </div><!--/.item-->
+        </div><!--/.carousel-inner-->
+        <a class="prev" href="#main-slider" data-slide="prev"><i class="icon-angle-left"></i></a>
+        <a class="next" href="#main-slider" data-slide="next"><i class="icon-angle-right"></i></a>
+    </section><!--/#main-slider-->
 
-	<!-- Main -->	
-	<table class="cartable">
-	<caption>Browse</caption>
-	<tbody>
-	<?php
+    <?php
 $con = mysqli_connect('localhost','root','','db_project');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
@@ -86,28 +82,69 @@ if (!$con) {
 mysqli_select_db($con, "db_project");
 $sql="SELECT purpose, type, city, title, price, size, unit,img, property_status FROM property";
 $result = mysqli_query($con,$sql);
-while($row = mysqli_fetch_array($result)){
-	?>
-	<h1>Browse</h1>
-<p>
-<?php
-			echo '<img class="float-left" src="data:image/jpeg;base64,' . base64_encode( $row['img'] ) . '" height="300" width="300" />';
-			?>
 
-			<?php echo $row['purpose']; ?> <br>
-			<?php echo $row['type']; ?><br>
-			<?php echo $row['city']; ?><br>
-			<?php echo $row['title']; ?><br>
-			<?php echo $row['price']; ?> <br>
-			<?php echo $row['size']; ?> <br>
-			<?php echo $row['unit']; ?> <br>
-			<?php echo $row['property_status']; ?> <br>
-</p>
-	<?php } ?>	
+	?>
+
+    <section id="portfolio">
+        <div class="container">
+            <div class="box">
+                <div class="center gap">
+                    <h2>Browse Properties</h2>
+                    <p class="lead">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac<br>turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
+                </div><!--/.center-->
+                <ul class="portfolio-filter">
+                    <li><a class="btn btn-primary active" href="#" data-filter="*">All</a></li>
+                    <li><a class="btn btn-primary" href="#" data-filter=".bootstrap">Bootstrap</a></li>
+                    <li><a class="btn btn-primary" href="#" data-filter=".html">HTML</a></li>
+                    <li><a class="btn btn-primary" href="#" data-filter=".wordpress">Wordpress</a></li>
+                </ul><!--/#portfolio-filter-->
+                <ul class="portfolio-items col-4">
+                	<?php while($row = mysqli_fetch_array($result)){ ?>
+                    <li class="portfolio-item apps">
+                        <div class="item-inner">
+                            <div class="portfolio-image">
+                                <?php	echo '<img class="float-left" width="370px" height="250px" src="data:image/jpeg;base64,' . base64_encode( $row['img'] ) . '" height="300" width="300" />'; ?>
+                                <div class="overlay">
+                                    <a class="preview btn btn-danger" title="<?php echo $row['purpose']; ?> <br>
+																		<?php echo $row['type']; ?><br>
+																		<?php echo $row['city']; ?><br>
+																		<?php echo $row['title']; ?><br>
+																		<?php echo $row['price']; ?> <br>
+																		<?php echo $row['size']; ?> <br>
+																		<?php echo $row['unit']; ?> <br>
+																		<?php echo $row['property_status']; ?> <br>" href="images/portfolio/full/item1.jpg"><i class="icon-eye-open"></i></a>             
+                                </div>
+                            </div>
+                            <h5><?php echo $row['title']; ?></h5>
+                        </div>
+                    </li><!--/.portfolio-item-->
+                    
+<?php } ?>	
 <?php	
 mysqli_close($con);
 ?>
-	</tbody>
-</table>
+                </ul>   
+            </div><!--/.box-->
+        </div><!--/.container-->
+    </section><!--/#portfolio-->    
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2013 <a target="_blank" href="http://shapebootstrap.net/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">ShapeBootstrap</a>. All Rights Reserved.
+                </div>
+                <div class="col-sm-6">
+                    <img class="pull-right" src="images/shapebootstrap.png" alt="ShapeBootstrap" title="ShapeBootstrap">
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
